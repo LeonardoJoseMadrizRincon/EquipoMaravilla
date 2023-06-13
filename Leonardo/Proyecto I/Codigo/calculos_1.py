efa312e80d2db398efde38952265cbc07361ca31
Leonardo Madriz <20-10399@usb.ve>
2023-06-12 22:34:38 -0400

    Codigo arreglado

--- Leonardo/Proyecto I/Codigo/calculos_1.py
+++ Leonardo/Proyecto I/Codigo/calculos_1.py
@@ -1,5 +1,6 @@
 import math
 import pandas
+import sys
 
 #Lectura de Archivo excel a trabajar
 filePath = "./valores_ascensor.xlsm"
@@ -8,13 +9,20 @@ df = pandas.read_excel("valores_ascensor.xls")
 # Esta función se encarga de realizar los calculos expuesta en el reglamento covenin 621 -3
 def calculus(n): 
 # Variables de entradas a tantear
-    z= float(df.iloc[n,1])  #Número de ascensores-->Excel       
-    p= float(df.iloc[n,2])  #Capacidad nominal-->Excel
-    vn= float(df.iloc[n,3]) #Velocidad nominal-->Excel
+    try:
+        z= float(df.iloc[n,1])  #Número de ascensores-->Excel       
+        p= float(df.iloc[n,2])  #Capacidad nominal-->Excel
+        vn= float(df.iloc[n,3]) #Velocidad nominal-->Excel
+        if p > 33:
+            raise Exception("Se sobrepasa los límites permitidos por el modelo del ascensor")
+    except Exception as ex:
+        print(ex)
+        sys.exit(1)
+
 
 
 # Cuentas obtenidas de los datos de cada grupo de ascensores
-    na = float(df.iloc[n,9])    #Pisos totales encima de la plataforma princial-->Excel
+    na = float(df.iloc[n,5])    #Pisos totales encima de la plataforma princial-->Excel
     ne = float(df.iloc[n,4])    #Pisos no atendidos-->Excel
     ep = 3.5                    #Altura entre pisos
     ns = na - ne                #Pisos recorridos
@@ -23,10 +31,16 @@ def calculus(n):
 
 
 #Valores pre-definidos por las reglas covenin, segun tablas
-    pv = float(df.iloc[n,5])    #Personas por viaje-->Excel/Tabla 5
-    np = float(df.iloc[n,6])    #Número de paradas-->Excel/Tabla 3
-    t1 = float(df.iloc[n,7])    #Tiempo -->Excel/Tabla 7
-    t2 = float(df.iloc[n,8])    #Excel/Tabla 8
+    try:
+        pv = int(3.2/p +0.7*p +0.5)    #Personas por viaje-->Excel/Tabla 5      
+        np = round(ns*(1-((ns-1)/ns)**pv),2)    #Número de paradas-->Excel/Tabla 3
+        t1 = float(df.iloc[n,6])    #Tiempo -->Excel/Tabla 7
+        t2 = float(df.iloc[n,7])    #Excel/Tabla 8
+        if np>250:
+            raise Exception("Se sobrepasó el número máximo de paradas recomendado por el modelo del ascensor")
+    except Exception as ex:
+        print(ex)
+        sys.exit(1)
 
 
 #Calculo de otras variables
@@ -42,6 +56,7 @@ def calculus(n):
 
     elif (check < vn) and (df.iloc[n,0] != "Grupo A"):
         tvc =2*(ha/vn)-hs/vn+2*(vn/phi)+(2*hs/(np*check))*(np-1) + t1*(np+1) + t2*pv
+
  
     elif (check >= vn) and (df.iloc[n,0] == "Grupo A"):
         tvc = 2*ha/vn + (vn/phi + t1)*(np + 1) + t2*pv
@@ -52,11 +67,11 @@ def calculus(n):
     else:
         print("Hay algo raro con la velocidad nominal")
 
-    ta = (1/10)*tvc #Tiempo adicional
+    ta = (3/10)*tvc #Tiempo adicional
     ttv = tvc + ta #Tiempo de un circuito completo
     i = ttv/z
-    c = (300*pv*(z*100))/(ttv*816)
-    return (c,i)
+    c = (300*pv*(z*100))/(ttv*B)
+    return (c,i,ns)
 
 
 def report(c,i,grupo,w):
@@ -87,18 +102,18 @@ def report(c,i,grupo,w):
 
 
 def run():
-    c1, i1 = calculus(0)
+    c1, i1, n1 = calculus(0)
     report(c1,i1,str(df.iloc[0,0]),"w")
 
 
-    c2, i2 = calculus(1)
+    c2, i2, n2 = calculus(1)
     report(c2,i2,str(df.iloc[1,0]),"a")
 
 
-    c3, i3 = calculus(2)
+    c3, i3, n3 = calculus(2)
     report(c3,i3,str(df.iloc[2,0]),"a")
 
-    print("El programa ha finalizado con éxitos!")
+    print("El programa a finalizado exitosamente!",n1+n2+n3)
 
 if __name__ == "__main__":
     run()
